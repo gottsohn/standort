65d1ca4e28e479592bf857450b53bc081c01f16b
andela-gukpere <godson.ukpere@andela.co>
2016-06-06 11:26:10 +0100

    [Updates #120845167] Add route and config services

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
-export default {
+module.exports = {
   isProduction: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
-  database: process.env.MONGODB_URL
+  database: process.env.MONGODB_URL || 'mongodb://localhost:27017/standort'
 };
