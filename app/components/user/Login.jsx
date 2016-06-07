a0184d2d702737a12212ae30a26f50aeb786c3b0
andela-gukpere <godson.ukpere@andela.co>
2016-06-07 02:23:54 +0100

    [Updates #120941971] Add firebase authentication methods to Login component

--- app/components/user/Login.jsx
+++ app/components/user/Login.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
+import firebase from '../shared/Firebase';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.authHandler = this.authHandler.bind(this);
+    this.state = {
+      user: null
+    };
+  }
+
+  authHandler(err, data) {
+    console.log(err, data);
+  }
+
+  handleFacebookAuth() {
+    console.log(this.authHandler, 'fn');
+    firebase.authWithOAuthPopup('facebook', (err, data)=> {
+      console.log(err, data);
+    });
+  }
+
+  handleGoogleAuth() {
+    firebase.authWithOAuthPopup('google', this.authHandler);
+  }
+
+  handleGitHubAuth() {
+    firebase.authWithOAuthPopup('github', this.authHandler);
   }
 
   render() {
     return (
       <div>
-        <h1>Einloggen</h1>
+        <h2>Login</h2>
+        <Divider/>
+        <p>Login with a social media account provided below</p>
+        <div>
+          <RaisedButton label="Google"  onTouchTap={this.handleGoogleAuth} />
+          <RaisedButton label="Facebook" onTouchTap={this.handleFacebookAuth} />
+          <RaisedButton label="Github"  onTouchTap={this.handleGitHubAuth} />
+        </div>
       </div>
     );
   }
