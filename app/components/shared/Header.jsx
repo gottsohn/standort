a9d125d1699b1f8bf2480856e985fcf0b1f3f752
andela-gukpere <godson.ukpere@andela.co>
2016-06-07 02:22:34 +0100

    [Updates #120843707] Add Material UI Appbar to Header

--- app/components/shared/Header.jsx
+++ app/components/shared/Header.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import {header} from '../../styles/App.css';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import {Link} from 'react-router';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import ActionHome from 'material-ui/svg-icons/action/home';
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -8,9 +15,28 @@ export default class Header extends React.Component {
 
   render() {
     return (
-      <nav className={header}>
-        <h1>Header</h1>
-      </nav>
-    );
+      <AppBar className={header}
+          iconElementLeft={
+            <Link to="/"><IconButton><ActionHome /></IconButton></Link>
+          }
+
+          iconElementRight={
+                  <IconMenu
+                      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                      iconButtonElement={
+                       <IconButton><MoreVertIcon /></IconButton>
+                      }
+                      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                  >
+                    <Link to="/login"><MenuItem primaryText="Login" /></Link>
+                    <Link to="/"><MenuItem primaryText="Help" /></Link>
+                  </IconMenu>
+            }
+          title={this.props.title}
+      />);
   }
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired
 }
