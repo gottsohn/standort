04205cd4f2cd16c92e7b9c6bb7f7bc1d6b8843f6
andela-gukpere <godson.ukpere@andela.co>
2016-06-09 23:10:04 +0100

    [Starts #121208177] Add dependencies and production config

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,14 +1,30 @@
-import WebpackStrip from 'strip-loader';
-import devConfig from './webpack.config.js';
+const WebpackStrip require('strip-loader'),
+  devConfig require('./webpack.config.js'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  StatsPlugin = require('stats-webpack-plugin');
 
-delete devConfig.plugins;
-delete devConfig.devtool;
-delete devConfig.entry;
+devConfig.plugins = devConfig.plugins.concat([
+  new ExtractTextPlugin(
+    '[name]-[hash].min.css'),
+
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false,
+      screw_ie8: true
+    }
+  }),
+
+  new StatsPlugin('webpack.stats.json', {
+    source: false,
+    modules: false
+  })
+])
+
 devConfig.devtool = 'source-map';
 devConfig.module.loaders.push({
-  test: /\.js$/,
+  test: /\.jsx$/,
   exclude: /node_modules/,
   loader: WebpackStrip.loader('console.log')
 });
 
-export default devConfig;
+module.exports = devConfig;
