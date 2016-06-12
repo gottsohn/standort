829a3be186604834e417833f4390bf90165fdc25
andela-gukpere <godson.ukpere@andela.co>
2016-06-12 02:27:22 +0100

    [#Delivers #121361729] Add authentication to pertinent components and scripts

--- app/App.js
+++ app/App.js
@@ -1,37 +1,99 @@
 import React from 'react';
 import Header from './components/shared/Header.jsx';
 import styles from './App.css';
+import firebase from './database';
+import SessionStore from './stores/SessionStore';
+import SessionActions from './actions/SessionActions';
 
 export default class App extends React.Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     title: 'Standort'
-   };
- }
-
- getChildContext() {
-    return {
-      title: this.state.title
+  constructor(props) {
+    super(props);
+    this.authCallback = this.authCallback.bind(this);
+    this.getCurrentUser = this.getCurrentUser.bind(this);
+    this.state = {
+      title: 'Standort',
+      user: {
+        id: null
+      }
     };
   }
 
- render() {
-    return (
-      <div>
-        <Header title={this.state.title} />
-        <div className={styles.container}>
-          {this.props.children}
-        </div>
-      </div>
-    );
+  getChildContext() {
+      return {
+        title: this.state.title,
+        user: this.state.user
+      };
   }
-}
 
-App.propTypes = {
-  children: React.PropTypes.element.isRequired
-};
+  componentDidMount() {
+    this.authCallback();
+  }
 
-App.childContextTypes = {
-  title: React.PropTypes.string.isRequired
-};
+  authCallback() {
+    firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        // A user is logged in
+        SessionStore.listen(this.getCurrentUser);
+        SessionActions.getSession();
+      } else {
+        // User is not logged or user is logged out
+        // Remove user from state
+        this.setState({
+          user: {
+            id: null
+          }
+        });
+      }
+    });
+  }
+
+  getCurrentUser(state) {
+    if (state.session) {
+      const userRef = firebase.database.ref(`users/${state.session.id}`);
+      // Save the user in the state
+      this.setState({
+        user: state.session
+      });
+
+      userRef.once('value', (snap) => {
+        if(!snap.val()) {
+          // Save user if user doesn't exist
+          userRef.set(state.session);
+        } else {
+          // Update user's photo, token an id for existing users
+          userRef.update({
+            photo: state.session.photo,
+            refreshToken: state.session.refreshToken,
+            uid: state.session.uid
+          });
+        }
+      });
+    }
+  }
+
+   render() {
+      return (
+        <div>
+          <Header title={this.state.title} />
+          <div className={styles.container}>
+            {this.props.children}
+          </div>
+        </div>
+      );
+    }
+  }
+
+  App.propTypes = {
+    children: React.PropTypes.element.isRequired
+  };
+
+  App.childContextTypes = {
+    title: React.PropTypes.string.isRequired,
+    user: React.PropTypes.shape({
+      name: React.PropTypes.string,
+      id: React.PropTypes.string,
+      uid: React.PropTypes.string,
+      photo: React.PropTypes.string,
+      email: React.PropTypes.string
+    })
+  };
--- app/database/index.js
+++ app/database/index.js
@@ -8,12 +8,13 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
 export default {
   database: app.database(),
   auth: app.auth(),
   providers: {
+    facebook: new firebase.auth.FacebookAuthProvider(),
     google: new firebase.auth.GoogleAuthProvider(),
-    facebook: new firebase.auth.facebookAuhProvider(),
     github: new firebase.auth.GithubAuthProvider()
   }
 };
