793f03d0807b12c1d3bba3c3152db9531cfacaf9
andela-gukpere <godson.ukpere@andela.co>
2016-06-16 17:30:54 +0100

    [Updates #120843707] Update path for pages

--- app/main.js
+++ app/main.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import {render} from 'react-dom';
-import App from './App.js';
-import Home from './components/shared/Home.jsx';
-import Login from './components/user/Login.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Profile from './components/user/Profile.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import App from './App.js';
+import Home from './components/pages/Home.jsx';
+import Search from './components/pages/Search.jsx';
+import Login from './components/pages/Login.jsx';
+import Profile from './components/pages/Profile.jsx';
+
 injectTapEventPlugin();
 
-render((
-<MuiThemeProvider muiTheme={getMuiTheme()}>
-  <Router history={browserHistory}>
-    <Route component={App} path="/">
-      <IndexRoute component={Home}/>
-      <Route component={Login} path="/login"/>
-      <Route component={Profile} path="/users/:id"/>
-    </Route>
-  </Router>
-</MuiThemeProvider>), document.getElementById('root'));
+render((<MuiThemeProvider muiTheme={getMuiTheme()}>
+          <Router history={browserHistory}>
+            <Route component={App} path="/">
+              <IndexRoute component={Home}/>
+              <Route component={Login} path="/login"/>
+              <Route component={Search} path="/search"/>
+              <Route component={Profile} path="/users/:id"/>
+            </Route>
+          </Router>
+        </MuiThemeProvider>),
+        document.getElementById('root'));
