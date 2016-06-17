1a89623e8dd2167593d28ebfe19ef4b1df08097b
andela-gukpere <godson.ukpere@andela.co>
2016-06-17 16:53:09 +0100

    [Finishes #121426831] Add Friend request and friend menus to Header

--- app/components/shared/Header.jsx
+++ app/components/shared/Header.jsx
@@ -9,6 +9,7 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationHome from 'material-ui/svg-icons/navigation/menu';
 import Avatar from 'material-ui/Avatar';
 import Divider from 'material-ui/Divider';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
 
 import styles from '../../App.css';
 import firebase from '../../database';
@@ -17,6 +18,7 @@ import PublicStore from '../../stores/PublicStore';
 import SessionActions from '../../actions/SessionActions';
 import LeftDrawer from './LeftDrawer.jsx';
 import DrawerActions from '../../actions/DrawerActions';
+import UserMenu from './UserMenu.jsx';
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -28,6 +30,8 @@ export default class Header extends React.Component {
     this.state = {
       user: null,
       title: 'Standort',
+      requests: [],
+      friends: [],
       isDrawerOpen: false
     };
   }
@@ -37,13 +41,40 @@ export default class Header extends React.Component {
     PublicStore.listen(this.setTitle);
   }
 
+  componentWillUnmount() {
+    SessionStore.unlisten(this.getCurrentUser);
+    PublicStore.unlisten(this.setTitle);
+  }
+
   handleSignOut() {
-    firebase.auth.signOut().then(SessionActions.getSession);
+    firebase.auth.signOut().then((err) => {
+      if (!err) {
+        SessionActions.getSession();
+      }
+    });
   }
 
   getCurrentUser(state) {
+    let requests = [],
+      friends = [],
+      user = state.session;
+
+    if (user) {
+      if (user.receivedRequests) {
+        requests = user.receivedRequests;
+        requests = Object.keys(requests).map(key => requests[key]);
+      }
+
+      if (user.friends) {
+        friends = user.friends;
+        friends = Object.keys(friends).map(key => friends[key]);
+      }
+    }
+
     this.setState({
-      user: state.session
+      user,
+      requests,
+      friends
     });
   }
 
@@ -57,27 +88,68 @@ export default class Header extends React.Component {
     });
   }
 
+  userMenu(user) {
+    return (
+      <UserMenu user={user} />
+    );
+  }
+
   render() {
     return (
       <div>
       <LeftDrawer />
       <AppBar className={styles.header}
           iconElementLeft = {
-            <IconButton onTouchTap={this.handleSideNavigation}><NavigationHome /></IconButton>
+            <IconButton onTouchTap={this.handleSideNavigation}>
+              <NavigationHome />
+            </IconButton>
           }
 
           iconElementRight = {
               <IconMenu
                   anchorOrigin={{horizontal: 'right', vertical: 'top'}}
                   iconButtonElement={
-                     this.state.user ? <Avatar src={this.state.user.photo} /> : <IconButton><MoreVertIcon /></IconButton>
+                     this.state.user ? <Avatar src={this.state.user.photo} /> :
+                        <IconButton><MoreVertIcon /></IconButton>
                   }
                   targetOrigin={{horizontal: 'right', vertical: 'top'}}
               >
-                {this.state.user ? <Link to={`/users/${this.state.user.id}`}><MenuItem primaryText="My Profile"/></Link> : null}
+                {
+                  this.state.user ?
+                  <Link to={`/users/${this.state.user.id}`}>
+                    <MenuItem primaryText="My Profile"/>
+                  </Link> : null
+                }
+                {
+                  this.state.user ?
+                  <MenuItem
+                      disabled={!this.state.friends.length}
+                      menuItems={this.state.friends.map(this.userMenu)}
+                      primaryText="My Friends"
+                      rightIcon={<ArrowDropRight />}
+                  /> : null
+                }
+                {
+                  this.state.user ?
+                  <MenuItem
+                      disabled={!this.state.requests.length}
+                      menuItems={this.state.requests.map(this.userMenu)}
+                      primaryText="Received Requests"
+                      rightIcon={<ArrowDropRight />}
+                  /> : null
+               }
                 <Link to="/"><MenuItem primaryText="Help" /></Link>
                 <Divider />
-                {this.state.user ? <MenuItem onTouchTap={this.handleSignOut} primaryText="Sign out"/> : <Link to="/login"><MenuItem primaryText="Login" /></Link>}
+                {
+                  this.state.user ?
+                  <MenuItem
+                      onTouchTap={this.handleSignOut}
+                      primaryText="Sign out"
+                  /> :
+                  <Link to="/login">
+                    <MenuItem primaryText="Login" />
+                  </Link>
+                }
 
               </IconMenu>
             }
