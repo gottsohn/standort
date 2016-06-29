55a74723f07d91708717c0ff9b3af45ac33b9b62
andela-gukpere <godson.ukpere@andela.co>
2016-06-29 17:44:57 +0100

    [Updates #123501791] Add search page link to homepage

--- app/components/pages/Home.jsx
+++ app/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 import PublicActions from '../../actions/PublicActions';
 
 export default class Home extends React.Component {
@@ -9,13 +10,14 @@ export default class Home extends React.Component {
   componentDidMount() {
     PublicActions.setTitle('Home');
   }
-  
+
   render() {
     return (
       <div>
         <h1>Haus</h1>
         <p>{this.context.title}</p>
         <p>Sehen dein context.</p>
+        <p>Find more friends <Link to="/search">here</Link>.</p>
       </div>
     );
   }
